73b5bb6367347104ec0a7872970917783c85d878
sirikanya.w <hachiiz@hotmail.com>
2017-03-31 16:21:55 +0700

    add bot.php

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-
+			responce($text);
 			$responce = makeResponceText($text);
 
 			responce($responce);
